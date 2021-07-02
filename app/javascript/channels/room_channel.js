89fc003270cd9227f5aee91a8c9f3889eaf81248
RoyNyaga <nyagaandreroy@gmail.com>
2021-07-02 11:44:08 +0100

    make channel dynamic to listen to other room ids

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,17 +1,34 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 2}, {
-  connected() {
-    console.log("connected....")
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener("turbolinks:load", () => {
+  const room_element = document.getElementById("room-id")
+  const room_id = Number(room_element.getAttribute("data-room-id"))
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id}, {
+    connected() {
+      console.log("connected to " + room_id)
+      // Called when the subscription is ready for use on the server
+    },
+  
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+  
+    received(data) {
+      console.log(data)
+      const user_element = document.getElementById("user-id")
+      const user_id = Number(user_element.getAttribute("data-user-id"))
+      let html;
+      if(user_id === data.message.user_id){
+        html = data.mine  
+        console.log("yes")
+      }else{
+        html = data.theirs
+        console.log("no")
+      }
+      console.log("user_id", user_id)
+      const messageContainer = document.getElementById("messages")
+      messageContainer.innerHTML = messageContainer.innerHTML + html 
+    }
+  });
+})
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    console.log(data)
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -1,7 +1,14 @@
 class SendMessageJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform(message)
+    mine = ApplicationController.render(
+      partial: "messages/mine", locals: {message: message} 
+    )
+    theirs = ApplicationController.render(
+      partial: "messages/theirs", locals: {message: message} 
+    )
+    ActionCable.server.broadcast "room_channel_#{message.room_id}", mine: mine, 
+    theirs: theirs, message: message
   end
 end
